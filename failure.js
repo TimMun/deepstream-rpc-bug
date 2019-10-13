b157a16c7573fc0d41d4d57873d5e4c54a0e2978
Tim Mun <tmun@berkeley.edu>
2019-10-12 18:49:58 -0700

    Updated for different bug

--- failure.js
+++ failure.js
@@ -1,70 +1,79 @@
 /**
- * This is a reproduction scenario for https://github.com/deepstreamIO/deepstream.io/issues/1001
+ * Repro case for https://github.com/deepstreamIO/deepstream.io-client-js/issues/510
  * 
+ * This case simulates a dataProvider that subscribes to presence changes for a testUser when he logs in (in the auth server).
+ * testUser logs in on two different clients using the same username.
  */
 
 var deepstreamClient = require("@deepstream/client")
 var { Deepstream } = require('@deepstream/server')
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
 
-// serverA and serverB are clustered by @deepstream/clusternode-redis
+
 // serverA on port 6020
 const serverA = new Deepstream('./config/ds-config1.yml')
 
-// serverB on port 6021
-const serverB = new Deepstream('./config/ds-config2.yml')
+const dataProvider = deepstreamClient('localhost:6020')
 
-// clientA to connect to serverA
 const clientA = deepstreamClient('localhost:6020')
-
-// clientB to connect to serverB
-const clientB = deepstreamClient('localhost:6021')
-
+const clientB = deepstreamClient('localhost:6020')
 
 serverA.start()
 
 serverA.on("started", () => {
 
-  clientA.login({ username: 'clientA' }, () => {
+  dataProvider.login({ username: 'dataProvider' }, () => {
 
-    clientA.rpc.provide("test-rpc", (data, response) => {
-      console.log("ClientA: test-rpc request successfully received")
-      response.send({})
-    })
+    // testUser logs in on first client
+    setTimeout(() => { clientA.login({ username: 'testUser' }) }, 2000 )
 
-    clientA.event.subscribe("test-event", () => {
-      console.log("ClientA: successfully received test-event")
-    })
+    // testUser logs in on second client
+    setTimeout(() => { clientB.login({ username: 'testUser' }) }, 4000)
 
-    /**
-     * This timeout simulates a second server node (serverB) joining the cluster, after clientA
-     * has already provided test-rpc on serverA. The bug seems to be that serverB doesn't request the 
-     * state registries of the existant nodes in the cluster; so when clientB makes the rpc, serverB
-     * doesn't route it to clientA, instead it responds with NO_RPC_PROVIDER.
-
-      Note that if you make the timeout small (~50ms on my pc), the error doesn't
-      occur because the rpc provide is registered after serverB joins the cluster.
-
-      Strangely enough, events seem to work; test-event will be received by clientA.
-     */
-
-    setTimeout(() => {
-      serverB.start()
-
-      serverB.on("started", () => {
-        clientB.login({ username: 'clientB' }, () => {
-          // event will be received by clientA
-          clientB.event.emit("test-event") 
-
-          // The test-rpc will fail with "NO_RPC_PROVIDER"
-          clientB.rpc.make("test-rpc", {}, (err, result) => {
-            if (err) {
-              console.log("test-rpc failure, err = " + err)
-            } else {
-              console.log("ClientB: test-rpc response successfully received")
-            }
-          })
-        })
-      })
-    }, 5000)
+    // testUser shuts down both clients. dataProvider will then error out.
+    setTimeout(() => { 
+      clientA.close()
+      clientB.close()
+    }, 8000)
   })
 })
+
+/**
+ * Auth Server
+ * 
+ * dataProvider subscribes to presence changes once user logs in
+ */
+app.use(bodyParser.json())
+
+app.post('/auth-user', (req, res) => {
+  if (req.body.authData.username) {
+    res.json({
+      username: req.body.authData.username,
+    })
+
+    // dataProvider subscribes to presence change if this is testUser
+    if (req.body.authData.username === 'testUser') {
+      console.log("Subscribing to presence change for testUser")
+      dataProvider.presence.subscribe('testUser', onPresenceChange)
+    }
+
+  } else {
+    res.status(403).send('Invalid Credentials')
+  }
+})
+
+app.listen(3000)
+
+
+
+var onPresenceChange = (username, active) => {
+  console.log("Presence changed: ", username, active)
+  if (!active) {
+    dataProvider.presence.unsubscribe(username, onPresenceChange)
+  }
+}
+
+
+
